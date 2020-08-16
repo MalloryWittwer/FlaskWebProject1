a68fad5bcccd9fc1447196742321b252b27c52df
MalloryWittwer <mallory.wittwer@gmail.com>
2020-08-16 17:23:45 +0800

    Setup Flask app (first steps)

--- FlaskWebProject1/runserver.py
+++ FlaskWebProject1/runserver.py
@@ -1,14 +1,12 @@
-"""
-This script runs the FlaskWebProject1 application using a development server.
-"""
-
-from os import environ
-from FlaskWebProject1 import app
+import os
+from HelloFlask import app    # Imports the code from HelloFlask/__init__.py
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+
     app.run(HOST, PORT)
